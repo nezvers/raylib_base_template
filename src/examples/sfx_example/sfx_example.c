806e187d7c2408e4f45427655bf9d43b87b62df3
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2026-07-12 14:57:02 +0300

    update sfx

--- src/examples/sfx_example/sfx_example.c
+++ src/examples/sfx_example/sfx_example.c
@@ -17,44 +17,45 @@ static void Gui();
                                /* Enter, Exit, Update, Draw, Gui, "Name" */
 AppState app_sfx_example = {Enter, Exit, Update, Draw, Gui, "SFX Example"};
 
-Sound button_sound;
-Sound damage_sound;
-
-SoundEffect button_sfx = {
-    .volume = 1.f,
-    .pitch_rand_min = 0.9f,
-    .pitch_rand_max = 1.2f,
-    .pitch_min = 0.5f,
-    .pitch_max = 1.75f,
-    .pitch_increment = 0.01f,     // Pitch change on fast retrigger
-    .retrigger_treshold = 0.02f,  // Don't play if sooner than this
-    .retrigger_interval = 0.5f,   // Applay pitch_increment
-    .pitch_return = 1,
+SoundEffectRaylib button_sfx = {
+    .sound_effect = {
+        .volume = 1.f,
+        .pitch_rand_min = 0.9f,
+        .pitch_rand_max = 1.2f,
+        .pitch_min = 0.5f,
+        .pitch_max = 1.75f,
+        .pitch_increment = 0.01f,     // Pitch change on fast retrigger
+        .retrigger_treshold = 0.02f,  // Don't play if sooner than this
+        .retrigger_interval = 0.5f,   // Applay pitch_increment
+        .pitch_return = 1,
+    }
 };
 
-SoundEffect damage_sfx = {
-    .volume = 0.5f,
-    .pitch_rand_min = 0.9f,
-    .pitch_rand_max = 1.2f,
-    .pitch_min = 0.5f,
-    .pitch_max = 1.75f,
-    .pitch_increment = 0.01f,
-    .retrigger_treshold = 0.02f,
-    .retrigger_interval = 0.5f,
-    .pitch_return = 1,
+SoundEffectRaylib damage_sfx = {
+    .sound_effect = {
+        .volume = 0.5f,
+        .pitch_rand_min = 0.9f,
+        .pitch_rand_max = 1.2f,
+        .pitch_min = 0.5f,
+        .pitch_max = 1.75f,
+        .pitch_increment = 0.01f,
+        .retrigger_treshold = 0.02f,
+        .retrigger_interval = 0.5f,
+        .pitch_return = 1,
+    }
 };
 
 
 static void Enter(){
     // InitAudioDevice(); // Should be initialized by Settings
     // RESOURCES_PATH is a macro definition from compiler
-    button_sound = LoadSound(RESOURCES_PATH "/sounds/button_sound.wav");
-    damage_sound = LoadSound(RESOURCES_PATH "/sounds/damage_sound.wav");
+    button_sfx.sound = LoadSound(RESOURCES_PATH "/sounds/button_sound.wav");
+    damage_sfx.sound = LoadSound(RESOURCES_PATH "/sounds/damage_sound.wav");
 }
 
 static void Exit(){
-    UnloadSound(button_sound);
-    UnloadSound(damage_sound);
+    UnloadSound(button_sfx.sound);
+    UnloadSound(damage_sfx.sound);
     // CloseAudioDevice(); // should be closed by Settings
 }
 
@@ -63,11 +64,11 @@ static void Update(){
     f32 rand_f = (GetRandomValue(0, 1000000) / 1000000.f) / 1.0f;
 
     if (IsKeyPressed(KEY_SPACE)) {
-        SoundEffectPlayRaylib(&button_sfx, &button_sound, current_time, rand_f);
+        SoundEffectPlayRaylib(&button_sfx, current_time, rand_f);
     }
 
     if (IsKeyPressed(KEY_ENTER)) {
-        SoundEffectPlayRaylib(&damage_sfx, &damage_sound, current_time, rand_f);
+        SoundEffectPlayRaylib(&damage_sfx, current_time, rand_f);
     }
 }
 
